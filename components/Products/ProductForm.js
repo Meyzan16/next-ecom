9fd96150f98afb37aa9c83d7807afc3faf077048
Meyzan16 <Meyzan1605@gmail.com>
2023-05-04 20:27:02 +0700

    create table products page

---
+++ components/Products/ProductForm.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+export default function ProductForm(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [goToProduct, setGoToProduct] = useState('');
+    const router = useRouter();
+
+    const createProduct = async (ev) => {
+        console.log('asas')
+        ev.preventDefault(); 
+        const data = {title,description,price};
+        await axios.post('/api/products', data);
+        setGoToProduct(true)
+    }
+
+    if(goToProduct){
+        router.push('/products');
+    }
+
+    return (
+            <div className="w-full lg:w-1/2">
+                <form onSubmit={createProduct}>
+                        <div>
+                            <label className="text-base">Full Name</label>
+                            <input  type="text" placeholder="Jane Doe"
+                            value={title} 
+                            onChange={ev => setTitle(ev.target.value) } />
+                        </div>
+                        <div>
+                            <label className="text-base">Description</label>
+                            <textarea placeholder="description"
+                            value={description} 
+                            onChange={ev => setDescription(ev.target.value) }
+                            />
+                        </div>
+
+                        <div>
+                            <label className="text-base">Price (in USD)</label>
+                            <input  type="number" placeholder="Jane Doe"
+                            value={price} 
+                            onChange={ev => setPrice(ev.target.value) }
+                             />
+                        </div>
+
+
+                        <Link href={'/products'} className="bg-red-700 py-2 px-4 text-sm text-white rounded-xl mr-2">Back</Link>
+
+                        <button type="submit" className="btn-primary">Save</button>
+                </form>
+ 
+            </div>
+
+    );
+}
--- pages/api/products.js
+++ pages/api/products.js
@@ -6,15 +6,21 @@ export default async function handler(req, res) {
     const {method} = req;
     await mongooseConnect();
 
+    if(method === 'GET') {
+        if(req.query?.id){
+            res.json(await Product.findOne({_id:req.query.id}))
+        }else{
+            res.json(await Product.find());
+        }
+
+    }
+
     if(method === 'POST'){
         const {title, description, price} = req.body;
         const productDoc =  await Product.create({
             title, description, price
         });
-
         res.json(productDoc);
-    }else{
-        console.log(error);
     }
 }
   
