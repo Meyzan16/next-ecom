95942886e108a910d84548a0e416998fcabb3af5
Meyzan16 <Meyzan1605@gmail.com>
2023-05-12 07:51:47 +0700

    crud bagian products

--- components/Products/ProductForm.js
+++ components/Products/ProductForm.js
@@ -8,6 +8,7 @@ export default function ProductForm({
     title:existingTitle, 
     description:existingDescription, 
     price:existingPrice,
+    image
 }){
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
@@ -22,16 +23,18 @@ export default function ProductForm({
         const data = {title,description,price};
         if(_id){
             //jika ada id maka update
-            await axios.put('/api/product', {...data, _id})
+            await axios.put('/api/products', {...data, _id})
         }else{
             await axios.post('/api/products', data);
-            setGoToProduct(true);
         }
+        setGoToProduct(true);
     }
 
     if(goToProduct){
         router.push('/products');
     }
+
+    
 
     return (
             <div className="w-full lg:w-1/2">
@@ -42,6 +45,25 @@ export default function ProductForm({
                             value={title} 
                             onChange={ev => setTitle(ev.target.value) } />
                         </div>
+
+                        <div>
+                            <label className="text-base">Photos</label>
+                            <label className="h-32 text-md cursor-pointer border-2 bg-gray-200 border-teal-500  text-gray-600 bg-transparent p-8 rounded-2xl w-full  flex items-center justify-center gap-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                </svg>
+                                <div>
+                                    Upload 
+                                </div>
+                                <input type="file"  className='hidden' onChange={uploadPhoto}/>
+                                    
+                            </label>
+                            {!image?.length && (
+                                <div>No photos in the product</div>
+                            )}
+                        </div>
+
+
                         <div>
                             <label className="text-base">Description</label>
                             <textarea placeholder="description"
